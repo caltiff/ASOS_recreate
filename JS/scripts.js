5348e68fba385c62550c54a4ffe8d78511f3c262
caltiff <65534412+caltiff@users.noreply.github.com>
2021-01-31 09:54:13 +0000

    Add files via upload

---
+++ JS/scripts.js
@@ -0,0 +1,22 @@
+
+
+    $(".click-me").click(function(){
+        $(".show-this").toggleClass('show');
+    });
+    
+
+    
+    $(".banner").hover(function(){
+        $(".tandc").toggleClass('show');
+    });
+
+
+    
+    $(".click-me").click(function(){
+        $(".show-this").toggleClass('container2');
+    });
+    
+    
+    $(".load").click(function(){
+        $(".load").toggleClass('container2');
+    });
